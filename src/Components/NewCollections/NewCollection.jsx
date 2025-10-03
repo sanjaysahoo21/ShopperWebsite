5dacd03b61010aaac5d89dec76c33793dd33fcf8
sanjaysahoo21 <sanjaysahoo2101@gmail.com>
2025-10-03 14:03:29 +0530

    modifiedn hero section and added items

---
+++ src/Components/NewCollections/NewCollection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewCollection.css'
+
+function NewCollection(props) {
+    return (
+        <div className='new-collections'>
+
+        </div>
+    );
+}
+
+export default NewCollection;
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
 
 const Shop = () => {
   return (
-    <div></div>
+    <div>
+        <Hero />
+        <Popular />
+        <Offers />
+    </div>
   )
 }
 
